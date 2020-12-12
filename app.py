5c690e8542d83931d29a64d4fa2dc7cedc1a1de2
Raedanwin <Raedanwin@gmail.com>
2020-12-12 15:41:45 -0500

    started auth stuff, cant use it because i changed focus

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify, g
+from flask_cors import CORS
+from flask_login import LoginManager
+
+import models
+
+login_manager = LoginManager()
 
 DEBUG = True
 PORT = 8000
 
+# from resources.users import user
+
 app = Flask(__name__)
+
+app.secret_key = 'thisisasecret'
+login_manager.init_app(app)
+
+# @login_manager.user_loader
+# def load_user(userid):
+#     try:
+#         return models.User.get(models.User.id == userid)
+#     except models.DoesNotExist:
+#         return None
+
+# @app.before_request
+# def before_request():
+#     g.db models.DATABASE
+#     g.db.connect()
+
+# @app.after_request
+# def after_request(response):
+#     g.db.close()
+#     return response
+
+# CORS(user, origins = '*', supports_credentials = True)
+
+# app.register_blueprint(user, user_prefix = '/user')
 
 @app.route('/')
 def index():
